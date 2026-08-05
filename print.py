44eb8e1fe91d1f9fee1b286d08bd11796a6f72c9
KeertiParawati <39194861+KeertiParawati@users.noreply.github.com>
2026-08-05 16:34:52 +0530

    Add files via upload

---
+++ print.py
@@ -0,0 +1,18 @@
+print("I am playing football with my friends",end="*")
+
+print("I am coding with my brother",end="*")
+
+print("I am playing zombie attack",end="*") 
+
+print("I am having a cycle race",end="*")
+
+print("I am doining a final WA",end="*")
+
+print("the addition of the two sum are",2+3,"\n")
+
+print("the multipication of the two sum are",2*3,"\n")
+
+print("the subtraction of the two sum are",2-3,"\n")
+
+print("the divsion of the two sum are",2/3,"\n")
+
