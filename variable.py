dba2546bcf12f5d640264cc8aeccbb3b793148cd
KeertiParawati <39194861+KeertiParawati@users.noreply.github.com>
2026-08-08 13:11:22 +0530

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,39 @@
+a = 1
+
+b = 3
+
+c = "vibhav"
+
+d = "vimarsh"
+
+e = "abeer"
+
+f = "sharvin"
+
+print("the value of a is",a)
+
+print("the value of b is",b)
+
+print("the value of c is",c) 
+
+print("the value of d is",d)
+
+print("the value of e is",e)
+
+print("the value of f is",f)
+
+vibhav = "12 jan"
+
+vimarsh = "26 sub"
+
+abeer = "7 marh"
+
+sharvin = "23 apr"
+
+print("the birth of vibhav is",vibhav)
+
+print("the birth of vimarsh is",vimarsh)
+
+print("the birth of abeer is",abeer)
+
+print("the birth of sharvin is",sharvin) 
