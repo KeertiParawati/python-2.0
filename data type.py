ce6a7f5d1cc494df78e45c09220458eaa0ff6d2e
KeertiParawati <39194861+KeertiParawati@users.noreply.github.com>
2026-08-12 16:32:47 +0530

    Add files via upload

---
+++ data type.py
@@ -0,0 +1,17 @@
+name = input("Enter your real name, Agent: ")
+gadget = input("Enter your favorite gadget: ")
+
+
+agent_number = 7
+speed_rating = 9.5
+mission_count = 12
+height_m = 1.65
+is_active = True
+
+print ("Name:", name, "-> type:", type(name))
+print("Gadget:", gadget, "-> type:", type(gadget))
+print("Agent Number:", agent_number, "-> type:", type(agent_number))
+print ("Speed Rating:", speed_rating, "-> type:", type(speed_rating))
+print("Mission Count:",mission_count,"->type:",type(mission_count))
+print("Height (m):", height_m, "-> type:", type(height_m))
+print("Is Active:", is_active, "-> type:", type(is_active))
