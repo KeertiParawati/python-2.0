de5d9e43c76f9e16d94bc29b068b6c4b7b63f74a
KeertiParawati <39194861+KeertiParawati@users.noreply.github.com>
2026-08-26 16:43:09 +0530

    Add files via upload

---
+++ identity is.py
@@ -0,0 +1,8 @@
+math = int(input("please enter your math marks"))
+evs = int(input("please enter your evs marks"))
+hindi = int(input("please enter your hindi marks"))
+english = int(input("plaese enter your english marks"))
+ctai = int(input("please enter your catai marks"))
+it = int(input("please enter your it marks"))
+total = math + evs + hindi + english + ctai + it
+print(total)
---
+++ identity.py
@@ -0,0 +1,18 @@
+x = 3.8
+if(type(x)is int):
+    print("true")
+else:
+    print("false") 
+
+a = 1
+b = 1 
+c = 9
+d = 9
+if(a is b):
+    print("true")
+else:
+    print("false")    
+if(c is not d):
+    print("true") 
+else:
+    print("false")                       
