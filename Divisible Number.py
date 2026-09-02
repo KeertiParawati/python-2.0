49ae5b1308ca74fdaaab135ec79b8b6eb7be984b
KeertiParawati <39194861+KeertiParawati@users.noreply.github.com>
2026-09-02 16:37:02 +0530

    Add files via upload

---
+++ Divisible Number.py
@@ -0,0 +1,9 @@
+print("Enter a Number(Numerator): ")
+numn = int (input())
+print("Enter a Number(Denominator): ")
+numd = int (input())
+
+if numn%numd ==0:
+    print("it is divisible")
+else:
+    print("it is not divisible")
