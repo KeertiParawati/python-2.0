edec8b5d17ccec0b7e953f4ce9a79094ba815ab5
KeertiParawati <39194861+KeertiParawati@users.noreply.github.com>
2026-09-09 16:34:53 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,14 @@
+n = int(input("Entrer the number:"))
+
+for i in range(1, n + 1):
+    print(i)
+#input an integer value 
+n = int(input("Enter a number whose sum you want to find:"))
+sum = 0
+#lterates for n+1 times i+1 to n+1:
+for i in range(1, n + 1):
+    sum = sum+i
+    print("\nSum =", sum)
+n = int(input("Please enter the number")) 
+for i in range(n, 0,-1):
+    print(i)
