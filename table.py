e362d9d53de1fc42814eb8a50ba8030418c1afa6
KeertiParawati <39194861+KeertiParawati@users.noreply.github.com>
2026-09-12 11:03:01 +0530

    Add files via upload

---
+++ table.py
@@ -0,0 +1,5 @@
+num = 2
+i = 1
+while num <= 10:
+    print(num,"x",i,"=",num * i)
+    i = i + 1 
---
+++ while loop.py
@@ -0,0 +1,25 @@
+i = 1
+while i <= 10: 
+    print(i)
+    i = i + 1 
+
+
+num = 1
+while num <= 20:
+    if num %2 == 0:
+        print(num)
+    num = num + 1  
+
+
+num = 1
+while num <= 20:
+    if num % 2 != 0:
+        print(num)
+    num = num + 1    
+
+
+num = 2
+i = 1
+while num <= 10:
+    print(num,"x",i,"=",num * i)
+    i = i + 1 
